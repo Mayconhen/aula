b366b99659412bd382ddf403e889c49b19f2d048
Maycon Henrique <mayconhenrique360@gmail.com>
2023-03-02 15:41:52 -0300

    atualizar

--- Mercado/caixa.py
+++ Mercado/caixa.py
@@ -3,5 +3,10 @@ from carteira import carteira
 
 produto1 = produto("Leite", "Desnatado", 100.0, 5)
 produto2 = produto("Biscoito", "chocolate", 50.0, 5)
+carteira1= carteira(100000.0)
 
-print(produto.__radd__(produto, produto))
+carteira.comprar(carteira1, produto2.valor)
+carteira.comprar(carteira1, produto1.valor)
+carteira.comprar(carteira1, produto2.valor)
+carteira.comprar(carteira1, produto1.valor)
+carteira1.saldototal
