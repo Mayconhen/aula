bcd9ecda03b1320b0d390c3bd4d163108afbfe57
Maycon Henrique <82100705+Mayconhen@users.noreply.github.com>
2023-03-01 22:22:46 -0300

    conta.py
    atualizar

--- conta.py
+++ conta.py
@@ -2,13 +2,49 @@ class conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Criando conta...")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite 
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite 
         
     def extrato(self):
-        print("O saldo de {} é de {}".format(self.titular, self.saldo))
+        print("O saldo de {} é de {} com o limite de{}".format(self.__titular, self.__saldo, self.__limite))
 
     def sacar(self, valor):
-        print("O saque total foi de {}".format(self.saldo - valor))
+        if valor > self.__saldo:
+            print("Saldo insuficiente")
+        else:
+            self.__saldo -= valor
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.deposita(valor)
+
+    @staticmethod
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @property
+    def get_limite(self):
+        return self.__limite
+    
+    @property
+    def saldo(self, valor):
+        self.__saldo = valor
+
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+        
+        
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from conta import conta
 
 conta1 = conta(1, "Maycon Henrique", 1000.0, 1200.0)
-valor = float(input("Digite o valor do saque: "))
-print(conta.extrato(conta1))
-print(conta.sacar(conta1, valor))
+
+
+conta1.extrato()
+conta1.sacar(1050)
+conta1.extrato()
+
